6ea19eac3a8ffcf64106745c9965b4c245c3a72d
robinjmc <robin.jmc@icloud.com>
2018-05-21 12:59:45 +0100

    seed fix

--- seed/seed.js
+++ seed/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {Article, Commment, Topic, User} = require('../models')
+const {Article, Comment, Topic, User} = require('../models')
 
 module.exports = {
     seedDB(articlesData, commentsData, topicsData, usersData) {
@@ -8,14 +8,38 @@ module.exports = {
             return Promise.all([Topic.insertMany(topicsData), User.insertMany(usersData)])
         })
         .then(([topicDocs, userDocs]) => {
-
-            console.log(topicDocs)
-
-        
-            return Promise.all(topicDocs, userDocs, Article.insertMany(articlesData))
+            articlesData.map(article => {
+                let topic = topicDocs.find(function(ele) {
+                    return ele.slug === article.topic
+                })
+                
+                let user = userDocs.find(function(ele) {
+                    return ele.username === article.created_by
+                })
+                
+                article.belongs_to = topic._id
+                article.created_by = user._id
+            })
+           
+            return Promise.all([topicDocs, userDocs, Article.insertMany(articlesData)])
         })
         .then(([topicDocs, userDocs, articleDocs]) => {
-            return Comment.insertMany(commentsData)
+            commentsData.map(comment => {
+                let article = articleDocs.find(function (ele) {
+                    return ele.title === comment.belongs_to
+                })
+                
+                let user = userDocs.find(function(ele) {
+                    return ele.username === comment.created_by
+                })
+                comment.belongs_to = article._id
+                comment.created_by = user._id
+            })
+            return Promise.all([topicDocs, userDocs, articleDocs, Comment.insertMany(commentsData)])
+        })
+        .then(data => {
+            console.log('database seeded!')
+            return data
         })
     }
 }
