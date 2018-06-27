b45d6f1225415757399642a47332b9695f8fb19a
robinjmc <robin.jmc@icloud.com>
2018-06-27 19:22:03 +0100

    installed and implemented cors

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const app = require('express')();
 const {json} = require('body-parser')
 const DB_URL = process.env.DB_URL || require('./config').DB_URL
 const api = require('./routes/api')
+const cors = require('cors')
+
 
 mongoose.connect(DB_URL)
     .then(() => {
         console.log(`connected to ${process.env.NODE_ENV} DB_URL`)
     })
+
+app.use(cors());
 
 app.use(json());
 
